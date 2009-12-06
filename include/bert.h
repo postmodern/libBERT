27e9bf9039e20b852ca99a5c351a26fed00f4c17
postmodern <postmodern.mod3@gmail.com>
2009-12-06 10:50:20 -0800

    Added bert_version().

---
+++ include/bert.h
@@ -0,0 +1,6 @@
+#ifndef _BERT_H_
+#define _BERT_H_
+
+extern const char * bert_version();
+
+#endif
---
+++ src/bert.c
@@ -0,0 +1,8 @@
+#include <bert.h>
+
+#define BERT_VERSION	"0.0.1"
+
+const char * bert_version()
+{
+	return BERT_VERSION;
+}
