1006c48ef7174e1f1b7bb304ef64edb479cb6ecf
postmodern <postmodern.mod3@gmail.com>
2009-12-07 09:47:06 -0800

    Added the bert/errno.h header.

---
+++ include/bert/errno.h
@@ -0,0 +1,10 @@
+#ifndef _BERT_ERRNO_H_
+#define _BERT_ERRNO_H_
+
+#define BERT_ERRNO_MALLOC	-3
+#define BERT_ERRNO_SHORT	-2
+#define BERT_ERRNO_INVALID	-1
+#define BERT_NOTHING		0
+#define BERT_SUCCESS		1
+
+#endif
