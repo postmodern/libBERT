be10fbf9132f9291dc2131a4572ff07d3a1f77ef
postmodern <postmodern.mod3@gmail.com>
2009-12-06 16:09:04 -0800

    Added BERT typedefs.

---
+++ include/bert/types.h
@@ -0,0 +1,13 @@
+#ifndef _BERT_TYPES_H_
+#define _BERT_TYPES_H_
+
+#include <sys/types.h>
+
+typedef uint16_t bert_string_size_t;
+typedef bert_string_size_t bert_atom_size_t;
+
+typedef uint32_t bert_binary_size_t;
+
+typedef uint32_t bert_tupel_size_t;
+
+#endif
