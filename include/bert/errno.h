f9e667d7378c3c35faeada0c4ff7af29a8c4cb68
postmodern <postmodern.mod3@gmail.com>
2009-12-07 16:46:21 -0800

    Renamed BERT_ERRNO_SHORT to BERT_ERRNO_EMPTY.

--- include/bert/errno.h
+++ include/bert/errno.h
@@ -1,8 +1,9 @@
 #ifndef _BERT_ERRNO_H_
 #define _BERT_ERRNO_H_
 
-#define BERT_ERRNO_MALLOC	-3
-#define BERT_ERRNO_SHORT	-2
+#define BERT_ERRNO_MALLOC	-4
+#define BERT_ERRNO_SHORT	-3
+#define BERT_ERRNO_EMPTY	-2
 #define BERT_ERRNO_INVALID	-1
 #define BERT_SUCCESS		0
 
