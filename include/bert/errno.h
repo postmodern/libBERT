21b4b0375687c5ec5d62667dbd4893cbb49b6a43
postmodern <postmodern.mod3@gmail.com>
2009-12-08 17:14:57 -0800

    Added bert_strerror.

--- include/bert/errno.h
+++ include/bert/errno.h
@@ -1,10 +1,13 @@
 #ifndef _BERT_ERRNO_H_
 #define _BERT_ERRNO_H_
 
+#define BERT_ERRNO_MAX		-5
 #define BERT_ERRNO_MALLOC	-4
 #define BERT_ERRNO_SHORT	-3
 #define BERT_ERRNO_EMPTY	-2
 #define BERT_ERRNO_INVALID	-1
 #define BERT_SUCCESS		0
+
+const char * bert_strerror(int errno);
 
 #endif
---
+++ src/errno.c
@@ -0,0 +1,20 @@
+#include <bert/errno.h>
+
+const char *bert_errors[] = {
+	"unknown bert error code",
+	"no more data left",
+	"not enough data available",
+	"malloc failed"
+};
+
+const char * bert_strerror(int errno)
+{
+	if (errno >= 0 || errno <= BERT_ERRNO_MAX)
+	{
+		return bert_errors[0];
+	}
+
+	unsigned int index = (-errno);
+
+	return bert_errors[index];
+}
