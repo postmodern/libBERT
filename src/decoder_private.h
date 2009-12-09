04a29b629c00f142641123515958c834a13d63d7
postmodern <postmodern.mod3@gmail.com>
2009-12-09 13:25:52 -0800

    Added prototype definitions for the private decoder functions.

--- src/decoder_private.h
+++ src/decoder_private.h
@@ -24,6 +24,23 @@ inline int32_t bert_decode_int32(bert_decoder_t *decoder);
 inline bert_magic_t bert_decode_magic(bert_decoder_t *decoder);
 inline int bert_decode_bytes(unsigned char *dest,bert_decoder_t *decoder,size_t length);
 
+inline int bert_decode_nil(bert_decoder_t *decoder,bert_data_t **data);
+inline int bert_decode_small_int(bert_decoder_t *decoder,bert_data_t **data);
+inline int bert_decode_big_int(bert_decoder_t *decoder,bert_data_t **data);
+int bert_decode_bignum(bert_decoder_t *decoder,bert_data_t **data,size_t size);
+inline int bert_decode_small_bignum(bert_decoder_t *decoder,bert_data_t **data);
+inline int bert_decode_big_bignum(bert_decoder_t *decoder,bert_data_t **data);
+inline int bert_decode_string(bert_decoder_t *decoder,bert_data_t **data);
+int bert_decode_time(bert_decoder_t *decoder,bert_data_t **data);
+int bert_decode_dict(bert_decoder_t *decoder,bert_data_t **data);
+int bert_decode_complex(bert_decoder_t *decoder,bert_data_t **data);
+int bert_decode_atom(bert_decoder_t *decoder,bert_data_t **data);
+inline int bert_decode_bin(bert_decoder_t *decoder,bert_data_t **data);
+inline int bert_decode_tuple(bert_decoder_t *decoder,bert_data_t **data,size_t size);
+inline int bert_decode_small_tuple(bert_decoder_t *decoder,bert_data_t **data);
+inline int bert_decode_big_tuple(bert_decoder_t *decoder,bert_data_t **data);
+int bert_decode_list(bert_decoder_t *decoder,bert_data_t **data);
+
 int bert_decoder_pull(bert_decoder_t *decoder,size_t size);
 
 #endif
