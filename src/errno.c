5d3aeaca5d7830743b15b38519ffa653f1884b2f
postmodern <postmodern.mod3@gmail.com>
2009-12-08 17:50:09 -0800

    Added the error message for BERT_ERRNO_INVALID.

--- src/errno.c
+++ src/errno.c
@@ -1,7 +1,8 @@
 #include <bert/errno.h>
 
 const char *bert_errors[] = {
-	"unknown bert error code",
+	"unknown libBERT error code",
+	"invalid BERT data",
 	"no more data left",
 	"not enough data available",
 	"malloc failed"
@@ -14,7 +15,7 @@ const char * bert_strerror(int errno)
 		return bert_errors[0];
 	}
 
-	unsigned int index = (-errno) - 1;
+	unsigned int index = (-errno);
 
 	return bert_errors[index];
 }
