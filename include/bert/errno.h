fbc9161c63778136b554399d132607e58c3cc80d
postmodern <postmodern.mod3@gmail.com>
2009-12-10 13:04:46 -0800

    Added the BERT_ERRNO_READ error code.
    * If the error code is BERT_ERRNO_READ, then have bert_strerror return
      the strerror of the global errno code.

--- include/bert/errno.h
+++ include/bert/errno.h
@@ -1,13 +1,14 @@
 #ifndef _BERT_ERRNO_H_
 #define _BERT_ERRNO_H_
 
-#define BERT_ERRNO_MAX		-5
-#define BERT_ERRNO_MALLOC	-4
+#define BERT_ERRNO_MAX		-6
+#define BERT_ERRNO_MALLOC	-5
+#define BERT_ERRNO_READ		-4
 #define BERT_ERRNO_SHORT	-3
 #define BERT_ERRNO_EMPTY	-2
 #define BERT_ERRNO_INVALID	-1
 #define BERT_SUCCESS		0
 
-const char * bert_strerror(int errno);
+const char * bert_strerror(int code);
 
 #endif
--- src/errno.c
+++ src/errno.c
@@ -1,21 +1,30 @@
 #include <bert/errno.h>
 
+#include <errno.h>
+#include <string.h>
+
 const char *bert_errors[] = {
 	"unknown libBERT error code",
 	"invalid BERT data",
 	"no more data left",
 	"not enough data available",
+	"read error",
 	"malloc failed"
 };
 
-const char * bert_strerror(int errno)
+const char * bert_strerror(int code)
 {
-	if (errno >= 0 || errno <= BERT_ERRNO_MAX)
+	if (code >= 0 || code <= BERT_ERRNO_MAX)
 	{
 		return bert_errors[0];
 	}
 
-	unsigned int index = (-errno);
+	if (code == BERT_ERRNO_READ)
+	{
+		return strerror(errno);
+	}
+
+	unsigned int index = (-code);
 
 	return bert_errors[index];
 }
