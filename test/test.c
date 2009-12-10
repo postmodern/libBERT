8c7680463cdf0acef29d5a6dde7f000c7e809fa3
postmodern <postmodern.mod3@gmail.com>
2009-12-09 18:41:09 -0800

    Fixed the passing of arguments to test_fail.

--- test/test.c
+++ test/test.c
@@ -1,9 +1,15 @@
+#include <stdarg.h>
+
 #include "test.h"
 
 void test_fail(const char *mesg,...)
 {
-	fprintf(stderr,mesg);
-	putchar('\n');
+	va_list args;
 
+	va_start(args,mesg);
+	vfprintf(stderr,mesg,args);
+	va_end(args);
+
+	putchar('\n');
 	exit(-1);
 }
