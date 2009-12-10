4edd338b9f588a71d76a9d0cb997d8d8c82afce2
postmodern <postmodern.mod3@gmail.com>
2009-12-09 17:36:52 -0800

    Added a test for performing small reads from a bert_buffer.

---
+++ test/test_buffer_small_read.c
@@ -0,0 +1,36 @@
+#include <bert/buffer.h>
+
+#include <string.h>
+#include <stdio.h>
+
+#include "test.h"
+
+#define DATA_SIZE	4
+
+int main()
+{
+	unsigned char data[4];
+	bert_buffer_t buffer;
+
+	bert_buffer_init(&buffer);
+
+	memset(data,'A',DATA_SIZE);
+	bert_buffer_write(&buffer,data,DATA_SIZE);
+
+	unsigned char output[DATA_SIZE];
+	size_t result;
+
+	if ((result = bert_buffer_read(output,&buffer,DATA_SIZE)) != DATA_SIZE)
+	{
+		test_fail("bert_buffer_read only read %u bytes, expected %u",result,DATA_SIZE);
+	}
+
+	int i;
+
+	if (memcmp(output,data,DATA_SIZE))
+	{
+		test_fail("bert_buffer_read return %c%c%c%c, expected AAAA",output[0],output[1],output[2],output[3]);
+	}
+
+	return 0;
+}
