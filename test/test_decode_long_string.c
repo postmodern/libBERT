33784bda8d805db760200b99297ad70be2692ba6
postmodern <postmodern.mod3@gmail.com>
2009-12-11 15:46:18 -0800

    Added a test for decoding long BERT strings.

---
+++ test/test_decode_long_string.c
@@ -0,0 +1,71 @@
+#include <bert/decoder.h>
+#include <bert/errno.h>
+
+#include "test.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+bert_decoder_t *decoder;
+
+void test_read()
+{
+	bert_data_t *data;
+	int result;
+
+	if ((result = bert_decoder_next(decoder,&data)) != 1)
+	{
+		test_fail(bert_strerror(result));
+	}
+
+	if (data->type != bert_data_string)
+	{
+		test_fail("bert_decoder_next did not decode a string");
+	}
+
+	size_t expected_length = 1025;
+
+	if (data->string.length != expected_length)
+	{
+		test_fail("bert_decoder_next decoded %u bytes, expected %u",data->string.length,expected_length);
+	}
+
+	unsigned int i;
+
+	for (i=0;i<expected_length-1;i++)
+	{
+		if (data->string.text[i] != 'A')
+		{
+			test_fail("data->string.text[%u] is %c, expected %c",i,data->string.text[i],'A');
+		}
+	}
+
+	if (data->string.text[expected_length-1] != 'B')
+	{
+		test_fail("data->string.text[%u] is %c, expected %c",expected_length-1,data->string.text[expected_length-1],'B');
+	}
+}
+
+int main()
+{
+	int fd;
+
+	if ((fd = open("files/long_string.bert",O_RDONLY)) == -1)
+	{
+		test_fail(strerror(errno));
+	}
+
+	if (!(decoder = bert_decoder_create()))
+	{
+		test_fail("malloc failed");
+	}
+
+	bert_decoder_stream(decoder,fd);
+
+	test_read();
+
+	bert_decoder_destroy(decoder);
+	close(fd);
+	return 0;
+}
