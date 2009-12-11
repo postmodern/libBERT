d332d46bb396da3865cc1316b3b9d1b00439fe8b
postmodern <postmodern.mod3@gmail.com>
2009-12-11 15:27:46 -0800

    Added a test for decoding BERT strings.

---
+++ test/test_decode_string.c
@@ -0,0 +1,62 @@
+#include <bert/decoder.h>
+#include <bert/errno.h>
+
+#include "test.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+bert_decoder_t *decoder;
+
+void test_read()
+{
+	bert_data_t *data;
+	int result;
+
+	if ((result = bert_decoder_next(decoder,&data)) != 1)
+	{
+		test_fail(bert_strerror(result));
+	}
+
+	if (data->type != bert_data_string)
+	{
+		test_fail("bert_decoder_next did not decode a string");
+	}
+
+	const char *expected = "hello world";
+	size_t expected_length = strlen(expected);
+
+	if (data->string.length != expected_length)
+	{
+		test_fail("bert_decoder_next decoded %u bytes, expected %u",data->string.length,expected_length);
+	}
+
+	if (memcmp(data->string.text,expected,expected_length))
+	{
+		test_fail("bert_decoder_next decoded %s, expected %s",data->string.text,expected);
+	}
+}
+
+int main()
+{
+	int fd;
+
+	if ((fd = open("files/string.bert",O_RDONLY)) == -1)
+	{
+		test_fail(strerror(errno));
+	}
+
+	if (!(decoder = bert_decoder_create()))
+	{
+		test_fail("malloc failed");
+	}
+
+	bert_decoder_stream(decoder,fd);
+
+	test_read();
+
+	bert_decoder_destroy(decoder);
+	close(fd);
+	return 0;
+}
