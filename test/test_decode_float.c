8923bd0c99dc96f5a6ec60ce57e2bd886076d83b
postmodern <postmodern.mod3@gmail.com>
2009-12-11 15:21:10 -0800

    Added a test for decoding floats.

---
+++ test/test_decode_float.c
@@ -0,0 +1,56 @@
+#include <bert/decoder.h>
+#include <bert/errno.h>
+
+#include "test.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+bert_decoder_t *decoder;
+
+void test_read()
+{
+	bert_data_t *data;
+	int result;
+
+	if ((result = bert_decoder_next(decoder,&data)) != 1)
+	{
+		test_fail(bert_strerror(result));
+	}
+
+	if (data->type != bert_data_float)
+	{
+		test_fail("bert_decoder_next did not decode a float");
+	}
+
+	double expected = 0.0000000005;
+
+	if (data->floating_point != expected)
+	{
+		test_fail("bert_decoder_next decoded %u, expected %u",data->integer,expected);
+	}
+}
+
+int main()
+{
+	int fd;
+
+	if ((fd = open("files/float.bert",O_RDONLY)) == -1)
+	{
+		test_fail(strerror(errno));
+	}
+
+	if (!(decoder = bert_decoder_create()))
+	{
+		test_fail("malloc failed");
+	}
+
+	bert_decoder_stream(decoder,fd);
+
+	test_read();
+
+	bert_decoder_destroy(decoder);
+	close(fd);
+	return 0;
+}
