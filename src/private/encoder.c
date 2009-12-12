2761c78012d043307469467c27b07ef77c72674e
postmodern <postmodern.mod3@gmail.com>
2009-12-11 19:59:04 -0800

    Added bert_encoder_write.

---
+++ src/private/encoder.c
@@ -0,0 +1,37 @@
+#include "encoder.h"
+#include <bert/errno.h>
+
+#include <unistd.h>
+#include <string.h>
+
+int bert_encoder_write(bert_encoder_t *encoder,const unsigned char *data,size_t length)
+{
+	switch (encoder->mode)
+	{
+		case bert_mode_stream:
+			if (write(encoder->stream,data,length) < 0)
+			{
+				return BERT_ERRNO_WRITE;
+			}
+			break;
+		case bert_mode_buffer:
+			if ((encoder->buffer.index + length) >= encoder->buffer.length)
+			{
+				return BERT_ERRNO_SHORT;
+			}
+
+			memcpy(encoder->buffer.ptr+encoder->buffer.index,data,sizeof(unsigned char)*length);
+			encoder->buffer.index += length;
+			break;
+		case bert_mode_callback:
+			if (encoder->callback.ptr(data,length,encoder->callback.data) < 0)
+			{
+				return BERT_ERRNO_INVALID;
+			}
+			break;
+		default:
+			return BERT_ERRNO_INVALID;
+	}
+
+	return BERT_SUCCESS;
+}
---
+++ src/private/encoder.h
@@ -0,0 +1,8 @@
+#ifndef _BERT_PRIVATE_ENCODER_H_
+#define _BERT_PRIVATE_ENCODER_H_
+
+#include <bert/encoder.h>
+
+int bert_encoder_write(bert_encoder_t *encoder,const unsigned char *data,size_t length);
+
+#endif
