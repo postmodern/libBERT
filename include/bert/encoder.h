0449a4b051c37115376bcf7c0816f157ad78bf94
postmodern <postmodern.mod3@gmail.com>
2009-12-11 18:06:23 -0800

    Stubbed out the API for bert_encoder.

---
+++ include/bert/encoder.h
@@ -0,0 +1,46 @@
+#ifndef _BERT_ENCODER_H_
+#define _BERT_ENCODER_H_
+
+#include <bert/types.h>
+#include <bert/data.h>
+
+#include <sys/types.h>
+
+typedef ssize_t (*bert_encoder_func)(const unsigned char *data,size_t length,void *data);
+
+struct bert_encoder
+{
+	bert_mode mode;
+
+	union
+	{
+		int stream;
+
+		struct
+		{
+			unsigned char *ptr;
+			size_t length;
+
+			unsigned int index;
+		} buffer;
+
+		struct
+		{
+			bert_encoder_func callback;
+			void *data;
+		} callback;
+	};
+};
+typedef struct bert_encoder bert_encoder_t;
+
+bert_encoder_t * bert_encoder_create();
+
+void bert_encoder_stream(bert_encoder_t *encoder,int stream);
+void bert_encoder_buffer(bert_encoder_t *encoder,unsigned char *buffer,size_t length);
+void bert_encoder_callback(bert_encoder_t *encoder,bert_encoder_func callback,void *data);
+
+size_t bert_encoder_sizeof(const bert_data_t *data);
+int bert_encoder_write(bert_encoder_t *encoder,const bert_data_t *data);
+void bert_encoder_destroy(bert_encoder_t *encoder);
+
+#endif
