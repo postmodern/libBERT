8aa2bb1dcf55b1b8f37abbf85d9085ad629e629b
postmodern <postmodern.mod3@gmail.com>
2009-12-11 18:52:34 -0800

    Removed bert_read_int8, bert_read_int16 and bert_read_int32.

--- src/util.c
+++ src/util.c
@@ -5,19 +5,9 @@ inline uint8_t bert_read_uint8(const unsigned char *ptr)
 	return (uint8_t)(*ptr);
 }
 
-inline int8_t bert_read_int8(const unsigned char *ptr)
-{
-	return (int8_t)(*ptr);
-}
-
 inline uint16_t bert_read_uint16(const unsigned char *ptr)
 {
 	return (uint16_t)((ptr[0] << 8) | ptr[1]);
-}
-
-inline int16_t bert_read_int16(const unsigned char *ptr)
-{
-	return (int16_t)((ptr[0] << 8) | ptr[1]);
 }
 
 inline bert_magic_t bert_read_magic(const unsigned char *ptr)
@@ -28,9 +18,4 @@ inline bert_magic_t bert_read_magic(const unsigned char *ptr)
 inline uint32_t bert_read_uint32(const unsigned char *ptr)
 {
 	return (uint32_t)((ptr[0] << 24 ) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3]);
-}
-
-inline int32_t bert_read_int32(const unsigned char *ptr)
-{
-	return (int32_t)((ptr[0] << 24 ) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3]);
 }
