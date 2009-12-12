ae857101340e92a0bc3c8eba404703ea40a2ace8
postmodern <postmodern.mod3@gmail.com>
2009-12-11 16:20:08 -0800

    Added a test for decoding long BERT bin strings.

---
+++ test/test_decode_long_bin.c
@@ -0,0 +1,71 @@
+#include <bert/decoder.h>
+#include <bert/errno.h>
+
+#include "test.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+bert_decoder_t *decoder;
+
+void test_read()
+{
+	bert_data_t *data;
+	int result;
+
+	if ((result = bert_decoder_next(decoder,&data)) != 1)
+	{
+		test_fail(bert_strerror(result));
+	}
+
+	if (data->type != bert_data_bin)
+	{
+		test_fail("bert_decoder_next did not decode bin data");
+	}
+
+	size_t expected_length = 1025;
+
+	if (data->bin.length != expected_length)
+	{
+		test_fail("bert_decoder_next decoded %u bytes, expected %u",data->string.length,expected_length);
+	}
+
+	unsigned int i;
+
+	for (i=0;i<expected_length-1;i++)
+	{
+		if (data->bin.data[i] != 'A')
+		{
+			test_fail("data->bin.data[%u] is %c, expected %c",i,data->bin.data[i],'A');
+		}
+	}
+
+	if (data->bin.data[expected_length-1] != 'B')
+	{
+		test_fail("data->bin.data[%u] is %c, expected %c",expected_length-1,data->bin.data[expected_length-1],'B');
+	}
+}
+
+int main()
+{
+	int fd;
+
+	if ((fd = open("files/long_bin.bert",O_RDONLY)) == -1)
+	{
+		test_fail(strerror(errno));
+	}
+
+	if (!(decoder = bert_decoder_create()))
+	{
+		test_fail("malloc failed");
+	}
+
+	bert_decoder_stream(decoder,fd);
+
+	test_read();
+
+	bert_decoder_destroy(decoder);
+	close(fd);
+	return 0;
+}
