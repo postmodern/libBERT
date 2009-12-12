d7f0f81037c0cbd1126a87d95a10ba5ff95fa81f
postmodern <postmodern.mod3@gmail.com>
2009-12-11 20:42:32 -0800

    Added some bert_write_* functions.

--- include/bert/util.h
+++ include/bert/util.h
@@ -12,4 +12,9 @@ inline uint16_t bert_read_uint16(const unsigned char *ptr);
 inline bert_magic_t bert_read_magic(const unsigned char *ptr);
 inline uint32_t bert_read_uint32(const unsigned char *ptr);
 
+inline void bert_write_uint8(unsigned char *dest,uint8_t i);
+inline void bert_write_uint16(unsigned char *dest,uint16_t i);
+inline void bert_write_uint32(unsigned char *dest,uint32_t i);
+inline void bert_write_magic(unsigned char *dest,bert_magic_t magic);
+
 #endif
--- src/util.c
+++ src/util.c
@@ -19,3 +19,27 @@ inline uint32_t bert_read_uint32(const unsigned char *ptr)
 {
 	return (uint32_t)((ptr[0] << 24 ) | (ptr[1] << 16) | (ptr[2] << 8) | ptr[3]);
 }
+
+inline void bert_write_uint8(unsigned char *dest,uint8_t i)
+{
+	dest[0] = i;
+}
+
+inline void bert_write_uint16(unsigned char *dest,uint16_t i)
+{
+	dest[0] = ((i & 0xff00) >> 8);
+	dest[1] = (i & 0xff);
+}
+
+inline void bert_write_uint32(unsigned char *dest,uint32_t i)
+{
+	dest[0] = ((i & 0xff000000) >> 24);
+	dest[1] = ((i & 0xff0000) >> 16);
+	dest[2] = ((i & 0xff00) >> 8);
+	dest[3] = (i & 0x00ff);
+}
+
+inline void bert_write_magic(unsigned char *dest,bert_magic_t magic)
+{
+	bert_write_uint8(dest,magic);
+}
