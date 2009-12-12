2465b37a86b991ab6c7ca7516d5eb27d4f2cd878
postmodern <postmodern.mod3@gmail.com>
2009-12-11 16:16:24 -0800

    Added a test for decoding BERT lists.

---
+++ test/test_decode_list.c
@@ -0,0 +1,78 @@
+#include <bert/decoder.h>
+#include <bert/errno.h>
+
+#include "test.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+bert_decoder_t *decoder;
+
+void test_read()
+{
+	bert_data_t *data;
+	int result;
+
+	if ((result = bert_decoder_next(decoder,&data)) != 1)
+	{
+		test_fail(bert_strerror(result));
+	}
+
+	if (data->type != bert_data_list)
+	{
+		test_fail("bert_decoder_next did not decode a list");
+	}
+
+	bert_list_node_t *next_node = data->list->head;
+	unsigned int i = 1;
+	unsigned int length = 0;
+
+	while (next_node)
+	{
+		if (next_node->data->type != bert_data_int)
+		{
+			test_fail("bert_decoder_next decoded a non-integer for list index %u",length);
+		}
+
+		if (next_node->data->integer != i)
+		{
+			test_fail("bert_decoder_next decoded integer %u for list index %u, expected %u",next_node->data->integer,length,i);
+		}
+
+		++i;
+		++length;
+
+		next_node = next_node->next;
+	}
+
+	unsigned int expected_length = 3;
+
+	if (length != expected_length)
+	{
+		test_fail("bert_decoder_next decoded %u list elements, expected %u",length,expected_length);
+	}
+}
+
+int main()
+{
+	int fd;
+
+	if ((fd = open("files/list.bert",O_RDONLY)) == -1)
+	{
+		test_fail(strerror(errno));
+	}
+
+	if (!(decoder = bert_decoder_create()))
+	{
+		test_fail("malloc failed");
+	}
+
+	bert_decoder_stream(decoder,fd);
+
+	test_read();
+
+	bert_decoder_destroy(decoder);
+	close(fd);
+	return 0;
+}
