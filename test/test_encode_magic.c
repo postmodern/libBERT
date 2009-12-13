27df0bed492832407e0b0963f9d0af8a51371b1e
postmodern <postmodern.mod3@gmail.com>
2009-12-12 19:16:02 -0800

    Added a test for encoding the BERT magic byte.

---
+++ test/test_encode_magic.c
@@ -0,0 +1,47 @@
+#include <bert/encoder.h>
+#include <bert/magic.h>
+#include <bert/errno.h>
+
+#include "test.h"
+
+unsigned char output[3];
+
+void test_output()
+{
+	if (output[0] != BERT_MAGIC)
+	{
+		test_fail("bert_encoder_push did not add the magic byte");
+	}
+}
+
+int main()
+{
+	bert_encoder_t *encoder;
+
+	if (!(encoder = bert_encoder_create()))
+	{
+		test_fail("malloc failed");
+	}
+
+	bert_encoder_buffer(encoder,output,3);
+
+	bert_data_t *data;
+
+	if (!(data = bert_data_create_int(0)))
+	{
+		test_fail("malloc failed");
+	}
+
+	int result;
+
+	if ((result = bert_encoder_push(encoder,data)) != BERT_SUCCESS)
+	{
+		test_fail(bert_strerror(result));
+	}
+
+	bert_data_destroy(data);
+	bert_encoder_destroy(encoder);
+
+	test_output();
+	return 0;
+}
