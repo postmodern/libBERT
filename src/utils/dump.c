e0f79735af3e4b62e9b5ab1be6441d92f0182487
postmodern <postmodern.mod3@gmail.com>
2009-12-18 14:04:37 -0800

    Fixed a format-string issue when using %lld and int64_t on systems using the LP64 model.

--- src/utils/dump.c
+++ src/utils/dump.c
@@ -208,7 +208,11 @@ int bert_print(const bert_data_t *data)
 			}
 			break;
 		case bert_data_int:
+#if (__WORDISZE == 64 || defined(__GLIBC_HAVE_LONG_LONG))
+			printf("%ld",data->integer);
+#else
 			printf("%lld",data->integer);
+#endif
 			break;
 		case bert_data_float:
 			printf("%15.15lf",data->floating_point);
