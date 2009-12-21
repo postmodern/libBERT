b6c86d4758d0cc9edbf6f223f04d64116ff715ae
postmodern <postmodern.mod3@gmail.com>
2009-12-20 23:56:25 -0800

    Added documentation to bert/list.h.

--- include/bert/list.h
+++ include/bert/list.h
@@ -20,8 +20,19 @@ struct bert_list
 };
 typedef struct bert_list bert_list_t;
 
+/*
+ * Allocates a new empty bert_list_t.
+ */
 extern bert_list_t * bert_list_create();
+
+/*
+ * Appends the given bert_data_t to the list.
+ */
 extern int bert_list_append(bert_list_t *list,struct bert_data *data);
+
+/*
+ * Destroys a previously allocated bert_list_t.
+ */
 extern void bert_list_destroy(bert_list_t *list);
 
 #endif
