963fa39d5324180db7a15e571e590a827aa8fc16
postmodern <postmodern.mod3@gmail.com>
2009-12-20 23:47:04 -0800

    Added documentation to bert/dict.h.

--- include/bert/dict.h
+++ include/bert/dict.h
@@ -21,8 +21,20 @@ struct bert_dict
 };
 typedef struct bert_dict bert_dict_t;
 
+/*
+ * Allocates a new empty bert_dict_t.
+ */
 extern bert_dict_t * bert_dict_create();
+
+/*
+ * Appends the given key and value pair to the bert_dict_t.
+ * Returns BERT_SUCCESS on success or BERT_ERRNO_MALLOC if malloc failed.
+ */
 extern int bert_dict_append(bert_dict_t *dict,struct bert_data *key,struct bert_data *value);
+
+/*
+ * Destroys a previously allocated bert_dict_t and it's contents.
+ */
 extern void bert_dict_destroy(bert_dict_t *dict);
 
 #endif
