56f81cfaa571b05279ba451a9ee70ddf84335076
postmodern <postmodern.mod3@gmail.com>
2009-12-20 23:44:03 -0800

    Added documentation to bert/decoder.h.

--- include/bert/decoder.h
+++ include/bert/decoder.h
@@ -39,14 +39,40 @@ struct bert_decoder
 };
 typedef struct bert_decoder bert_decoder_t;
 
+/*
+ * Allocates a new bert_decoder_t and sets the mode to bert_mode_none.
+ */
 bert_decoder_t * bert_decoder_create();
 
+/*
+ * Sets the mode of the given decoder to bert_mode_stream, and uses the given
+ * file descriptor as the stream to read BERT encoded data from.
+ */
 void bert_decoder_stream(bert_decoder_t *decoder,int fd);
+
+/*
+ * Sets the mode of the given decoder to bert_mode_callback, and uses the
+ * given bert_read_func as the callback to read BERT encoded data from.
+ */
 void bert_decoder_callback(bert_decoder_t *decoder,bert_read_func callback,void *data);
+
+/*
+ * Sets the mode of the given decoder to bert_mode_buffer, and uses the
+ * given buffer as the BERT encoded data to decode.
+ */
 void bert_decoder_buffer(bert_decoder_t *decoder,const unsigned char *buffer,size_t length);
 
+/*
+ * Reads BERT encoded data from the decoder and attempts to decode it.
+ * Points the given data_ptr to the newly decoded bert_data_t.
+ * Returns 1 when a bert_data_t has been decoded or 0 when there is no more
+ * data to decode.
+ */
 extern int bert_decoder_pull(bert_decoder_t *decoder,bert_data_t **data_ptr);
 
+/*
+ * Destroys a previously allocated bert_decoder_t.
+ */
 extern void bert_decoder_destroy(bert_decoder_t *decoder);
 
 #endif
