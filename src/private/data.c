d41bc00b23415840cc9852867025ce77596e4d5d
postmodern <postmodern.mod3@gmail.com>
2009-12-25 19:26:36 -0800

    Properly calculate size of BERT bignums.

--- src/private/data.c
+++ src/private/data.c
@@ -14,23 +14,38 @@ size_t bert_data_sizeof_int(int64_t i)
 			return 4;
 		}
 	}
+
+	uint64_t unsigned_integer = BERT_STRIP_SIGN(i);
+	size_t byte_count = 0;
+	unsigned int j;
+
+	for (j=sizeof(int64_t)-1;j>=0;j--)
+	{
+		if (unsigned_integer & (0xff << (j * 8)))
+		{
+			byte_count = (j + 1);
+			break;
+		}
+	}
+
+	size_t count = 0;
+
+	if (byte_count > 0xff)
+	{
+		// byte length
+		count += 4;
+	}
 	else
 	{
-		// byte length + signed byte
-		size_t count = 1 + 1;
-
-		uint64_t unsigned_integer = BERT_STRIP_SIGN(i);
-		unsigned int j;
-
-		for (j=sizeof(int64_t)-1;j>=0;j--)
-		{
-			if (unsigned_integer & (0xff << (j * 8)))
-			{
-				count += (j + 1);
-				break;
-			}
-		}
-
-		return count;
+		// byte length
+		++count;
 	}
+
+	// signed byte
+	++count;
+
+	// additional bytes
+	count += byte_count;
+
+	return count;
 }
