3f727a67227e83ae9e101de4343e85b345b8765d
postmodern <postmodern.mod3@gmail.com>
2010-01-09 03:17:34 -0800

    Added extern keywords to bert/encoder.h.

--- include/bert/encoder.h
+++ include/bert/encoder.h
@@ -38,25 +38,25 @@ typedef struct bert_encoder bert_encoder_t;
 /*
  * Allocates a new bert_encoder_t.
  */
-bert_encoder_t * bert_encoder_create();
+extern bert_encoder_t * bert_encoder_create();
 
 /*
  * Sets the mode of the given encoder to bert_mode_stream, and uses the
  * given file descriptor as the stream to write BERT encoded data to.
  */
-void bert_encoder_stream(bert_encoder_t *encoder,int stream);
+extern void bert_encoder_stream(bert_encoder_t *encoder,int stream);
 
 /*
  * Sets the mode of the given encoder to bert_mode_buffer, and uses the
  * given buffer to write BERT encoded data to.
  */
-void bert_encoder_buffer(bert_encoder_t *encoder,unsigned char *buffer,size_t length);
+extern void bert_encoder_buffer(bert_encoder_t *encoder,unsigned char *buffer,size_t length);
 
 /*
  * Sets the mode of the given encoder to bert_mode_callback, and uses the
  * given callback to write BERT encoded data to.
  */
-void bert_encoder_callback(bert_encoder_t *encoder,bert_write_func callback,void *data);
+extern void bert_encoder_callback(bert_encoder_t *encoder,bert_write_func callback,void *data);
 
 /*
  * Encodes the given bert_data_t and writes it to the encoder.
@@ -66,16 +66,16 @@ void bert_encoder_callback(bert_encoder_t *encoder,bert_write_func callback,void
  *   encoded BERT data.
  * Returns BERT_ERRNO_WRITE if a call to write() failed.
  */
-int bert_encoder_push(bert_encoder_t *encoder,const bert_data_t *data);
+extern int bert_encoder_push(bert_encoder_t *encoder,const bert_data_t *data);
 
 /*
  * Returns the number of bytes written so far.
  */
-size_t bert_encoder_total(const bert_encoder_t *encoder);
+extern size_t bert_encoder_total(const bert_encoder_t *encoder);
 
 /*
  * Destroys a previously allocated bert_encoder_t.
  */
-void bert_encoder_destroy(bert_encoder_t *encoder);
+extern void bert_encoder_destroy(bert_encoder_t *encoder);
 
 #endif
