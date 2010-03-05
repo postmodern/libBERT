4401e95944ba7cee1de240143e48ce681590c5a7
postmodern <postmodern.mod3@gmail.com>
2010-03-05 13:26:55 -0800

    Updated the top-level bert.h.

--- include/bert.h
+++ include/bert.h
@@ -1,6 +1,12 @@
 #ifndef _BERT_H_
 #define _BERT_H_
 
+#include <bert/config.h>
+#include <bert/data.h>
+#include <bert/decoder.h>
+#include <bert/encoder.h>
+#include <bert/errno.h>
+
 extern const char * bert_version();
 
 #endif
